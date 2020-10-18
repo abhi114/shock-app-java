a6197d635aefe5a7e6e99a1df262aff136221a92
abhi114 <nabhi531@gmail.com>
2020-10-19 02:37:26 +0530

    jump-activity-added

--- app/src/main/java/com/example/shock_app/MainActivity.java
+++ app/src/main/java/com/example/shock_app/MainActivity.java
@@ -15,36 +15,16 @@ import com.bumptech.glide.Glide;
 import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
-    ImageView imageView ;
-    Uri photoUri;
-    Uri soundUri;
 
-    TextToSpeech tts;
-    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        imageView = findViewById(R.id.imageView);
 
-        //imageView.setImageResource();
-        photoUri = Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE + File.pathSeparator + File.separator + File.separator + "/drawable/bust_1");
-        soundUri = Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE + File.pathSeparator + File.separator + File.separator + "/raw/scream2");
-
-        showImage();
-        playSoundClip();
 
     }
-    private void showImage(){
-        Glide.with(this).load(photoUri).into(imageView);
-        imageView.setVisibility(View.VISIBLE);
-    }
-    private void playSoundClip(){
-        mediaPlayer = MediaPlayer.create(this,soundUri);
-        mediaPlayer.start();
 
-    }
 
 }
--- app/src/main/java/com/example/shock_app/SupriseActivity.java
+++ app/src/main/java/com/example/shock_app/SupriseActivity.java
@@ -2,13 +2,59 @@ package com.example.shock_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentResolver;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+
+import java.io.File;
 
 public class SupriseActivity extends AppCompatActivity {
+    ImageView imageView ;
+    Uri photoUri;
+    Uri soundUri;
+
+    TextToSpeech tts;
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_suprise);
+
+        imageView = findViewById(R.id.imageView);
+
+        //imageView.setImageResource();
+        photoUri = Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE + File.pathSeparator + File.separator + File.separator + getPackageName() + "/drawable/bust_1");
+        soundUri = Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE + File.pathSeparator + File.separator + File.separator + getPackageName() + "/raw/scream2");
+
+        Toast.makeText(this,"ready",Toast.LENGTH_SHORT).show();
+
+    }
+
+    private void showImage(){
+        Glide.with(this).load(photoUri).into(imageView);
+        imageView.setVisibility(View.VISIBLE);
+    }
+    private void playSoundClip(){
+        mediaPlayer = MediaPlayer.create(this,soundUri);
+        mediaPlayer.start();
+
+    }
+
+    private void userTriggerActions(){
+        
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return super.onTouchEvent(event);
     }
 }
