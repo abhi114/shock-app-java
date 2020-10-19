7ca912875ea7c1f245f4ff7e85d72b1fb9c477a1
abhi114 <nabhi531@gmail.com>
2020-10-20 02:10:50 +0530

    notification added

--- app/src/main/java/com/example/shock_app/MainActivity.java
+++ app/src/main/java/com/example/shock_app/MainActivity.java
@@ -1,12 +1,21 @@
 package com.example.shock_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -22,9 +31,45 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        findViewById(R.id.prankSurface).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createNotification();
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu,menu);
+        return true;
+    }
+    private void createNotification(){
+        String notificationMessage = "Tap to Shock Friends";
+        int reqId = (int) System.currentTimeMillis();
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        Intent notificationIntent = new Intent(this,SupriseActivity.class);
+        notificationIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        notificationIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+        PendingIntent contentIntent = PendingIntent.getActivity(this,reqId,notificationIntent,PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher).setStyle(new NotificationCompat.BigTextStyle().bigText(notificationMessage))
+                .setContentText(notificationMessage).setAutoCancel(true)
+                .setContentIntent(contentIntent);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            String channelId = "Anything Channel";
+            NotificationChannel channel = new NotificationChannel(channelId,"Channel Human Readable title",NotificationManager.IMPORTANCE_DEFAULT);
+            notificationManager.createNotificationChannel(channel);
+            builder.setChannelId(channelId);
+
+        }
+        notificationManager.notify(12312,builder.build());
 
 
     }
-
-
 }
