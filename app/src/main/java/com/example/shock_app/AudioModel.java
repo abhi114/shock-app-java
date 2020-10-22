b7d22bbf393b45cb91c710f60dd4bac3076bc9b0
abhi114 <nabhi531@gmail.com>
2020-10-22 22:10:45 +0530

    audio storer complete

--- app/src/main/java/com/example/shock_app/AudioModel.java
+++ app/src/main/java/com/example/shock_app/AudioModel.java
@@ -1,6 +1,64 @@
 package com.example.shock_app;
 
-public class AudioModel {
+import java.io.Serializable;
+import java.util.Objects;
+
+public class AudioModel implements Serializable {
     int id;
     String audioFileName;
+    String descriptionMessage;
+    boolean isAsset;
+    boolean isTTS;  // text to speech
+
+    //for asset audio only
+    public AudioModel(int id, String audioFileName, String descriptionMessage, boolean isAsset) {
+        this.id = id;
+        this.audioFileName = audioFileName;
+        this.descriptionMessage = descriptionMessage;
+        this.isAsset = isAsset;
+    }
+
+    // for text to speech only
+    public AudioModel(int id, String descriptionMessage) {
+        this.id = id;
+        this.descriptionMessage = descriptionMessage;
+    }
+
+    //getters
+    public int getId() {
+        return id;
+    }
+
+    public String getAudioFileName() {
+        return audioFileName;
+    }
+
+    public String getDescriptionMessage() {
+        return descriptionMessage;
+    }
+
+    public boolean isAsset() {
+        return isAsset;
+    }
+
+    public boolean isTTS() {
+        return isTTS;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AudioModel that = (AudioModel) o;
+        return id == that.id &&
+                isAsset == that.isAsset &&
+                isTTS == that.isTTS &&
+                Objects.equals(audioFileName, that.audioFileName) &&
+                Objects.equals(descriptionMessage, that.descriptionMessage);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, audioFileName, descriptionMessage, isAsset, isTTS);
+    }
 }
---
+++ app/src/main/java/com/example/shock_app/AudioStorer.java
@@ -0,0 +1,73 @@
+package com.example.shock_app;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AudioStorer {
+
+    Context context;
+    SharedPreferences preferences;
+    SharedPreferences.Editor editor;
+
+    public AudioStorer(Context context){
+        this.context = context;
+        preferences = context.getSharedPreferences(ShockUtils.SHOCK_SCARED_PREFS,Context.MODE_PRIVATE);
+        editor = preferences.edit();
+
+    }
+
+    public void storeAudios(List<AudioModel> audioModelList){
+        String key = context.getString(R.string.key_stored_audios);
+        Gson gson = new Gson();
+        editor.putString(key,gson.toJson(audioModelList));
+        editor.commit();
+    }
+
+    private List<AudioModel> getStoredAudios(){
+        String audiosAsString = preferences.getString(context.getString(R.string.key_stored_audios),null);
+        if(audiosAsString == null || audiosAsString.length() == 0){
+            return new ArrayList<>();
+
+        }
+        Gson gson = new Gson();
+        Type type = new TypeToken<List<AudioModel>>(){}.getType();
+        return gson.fromJson(audiosAsString,type);
+
+    }
+
+    public List<AudioModel> getAllAudios(){
+        List<AudioModel> audios = new ArrayList<>();
+        audios.add(new AudioModel(0,"Scream2","Scream 2",true));
+        audios.add(new AudioModel(1,"behind_you","behind you",true));
+        audios.add(new AudioModel(2,"Scream1","Scream 1",true));
+
+        audios.addAll(getStoredAudios());
+        return audios;
+    }
+
+    public AudioModel getSelectedAudio(){
+        List<AudioModel> audios = getAllAudios();
+
+        AudioModel defaultAudio = audios.get(0);
+
+        int audioId = preferences.getInt(context.getString(R.string.key_audio_id),0);
+        for(AudioModel audio : audios){
+            if(audio.getId() == audioId){
+                return audio;
+            }
+        }
+        //fallback on default
+        editor.putInt(context.getString(R.string.key_audio_id),0);
+        editor.commit();
+        return  defaultAudio;
+    }
+
+
+}
