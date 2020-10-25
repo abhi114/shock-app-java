81d7760810a1ea32b4270a5450094ad022afa0a7
abhi114 <nabhi531@gmail.com>
2020-10-25 19:41:59 +0530

    add image_item.xml

--- app/src/main/java/com/example/shock_app/ImagePickerAdapter.java
+++ app/src/main/java/com/example/shock_app/ImagePickerAdapter.java
@@ -1,4 +1,29 @@
 package com.example.shock_app;
 
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+// refer to the audio adapter for comments
 public class ImagePickerAdapter {
+
+    class ViewHolder extends RecyclerView.ViewHolder{ //
+        ImageView imageView;
+
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+    //interface for callback
+    //used to notify the dialog fragment that something had happend
+    interface Callback{
+        //it cannot have a body
+        void itemSelected(ImageModel item);
+    }
+
+
+
 }
